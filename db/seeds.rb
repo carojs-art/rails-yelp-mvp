8bfb9a0d0e76fdfff78700ea56757abcdb35a910
caroline janin <caroline.js@icloud.com>
2020-08-12 18:19:10 +0200

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+hola = { name: "Hola", address: "7 rue d'Espagne, Madrid ", phone_number: '0101010101' }
+welcome = { name: "Welcome", address: "9 rue d'angleterre, London ", phone_number: '0202020202' }
+bonjour = { name: "Bonjour", address: "12 rue de France, Paris ", phone_number: '0303030303' }
+gutentag = { name: "Gutentag", address: "3 rue d'Allemagne, Berlin ", phone_number: '0404040404' }
+willkommen = { name: "Willkommen", address: "1 rue d'Hollande, Amsterdam ", phone_number: '0505050505' }
+
+[hola, welcome, bonjour, gutentag, willkommen].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
